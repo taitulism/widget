73d56aff1acb1bf5cd537df537180807e7b944c6
Itai Tenenbaum <taitulism@gmail.com>
2020-10-07 02:04:37 +0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,11 @@ module.exports = {
 	env: {
 		browser: true,
 		commonjs: true,
-		es6: true,
 		mocha: true,
 	},
 	extends: 'eslint:recommended',
-	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly'
-	},
 	parserOptions: {
-		ecmaVersion: 2018,
+		ecmaVersion: 6,
 		sourceType: 'module'
 	},
 	overrides: [
