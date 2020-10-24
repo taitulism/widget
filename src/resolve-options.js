a3f56b838996877170a19636b934e6c11c502f35
Itai Tenenbaum <taitulism@gmail.com>
2020-10-24 20:23:26 +0300

    improve resolve-options fn

--- src/resolve-options.js
+++ src/resolve-options.js
@@ -1,4 +1,4 @@
-/* eslint-disable complexity, max-len, no-nested-ternary */
+/* eslint-disable complexity, no-nested-ternary */
 
 const DEFAULT_MIN_WIDTH = 180;
 const DEFAULT_MIN_HEIGHT = 110;
@@ -12,46 +12,52 @@ const isNumber = thing => typeof thing == NUMBER;
 const isBoolean = thing => typeof thing == BOOLEAN;
 const isString = thing => typeof thing == STRING;
 
+const defaultOpts = {
+	id: null,
+	classname: null,
+	showHeader: true,
+	showActions: true,
+	showClose: true,
+	showMinimize: true,
+	showMaximize: true,
+	toggleHeader: false,
+	toggleActions: false,
+	minWidth: DEFAULT_MIN_WIDTH,
+	minHeight: DEFAULT_MIN_HEIGHT,
+	top: DEFAULT_POSITION,
+	bottom: null,
+	left: null,
+	right: DEFAULT_POSITION,
+};
+
 export default function resolveOptions (rawOpts) {
-	if (!rawOpts) {
-		return {
-			id: null,
-			classname: null,
-			showHeader: true,
-			showActions: true,
-			showClose: true,
-			showMinimize: true,
-			showMaximize: true,
-			toggleHeader: false,
-			toggleActions: false,
-			minWidth: DEFAULT_MIN_WIDTH,
-			minHeight: DEFAULT_MIN_HEIGHT,
-			top: DEFAULT_POSITION,
-			bottom: null,
-			left: null,
-			right: DEFAULT_POSITION,
-		};
-	}
+	if (!rawOpts) return defaultOpts;
+
+	const { id, classname, showHeader, showActions, showClose, showMinimize, showMaximize,
+		toggleHeader, toggleActions, minWidth, minHeight, top, bottom, left, right,
+	} = rawOpts;
 
 	return {
-		id: rawOpts.id && isString(rawOpts.id) ? rawOpts.id : null,
-		classname: isString(rawOpts.classname) || Array.isArray(rawOpts.classname) ? rawOpts.classname : null,
-		showHeader: isBoolean(rawOpts.showHeader) ? rawOpts.showHeader : true,
-		showActions: isBoolean(rawOpts.showActions) ? rawOpts.showActions : true,
-		showClose: isBoolean(rawOpts.showClose) ? rawOpts.showClose : true,
-		showMinimize: isBoolean(rawOpts.showMinimize) ? rawOpts.showMinimize : true,
-		showMaximize: isBoolean(rawOpts.showMaximize) ? rawOpts.showMaximize : true,
-		toggleHeader: isBoolean(rawOpts.toggleHeader) ? rawOpts.toggleHeader : false,
-		toggleActions: isBoolean(rawOpts.toggleActions) ? rawOpts.toggleActions : false,
-		minWidth: isNumber(rawOpts.minWidth) ? rawOpts.minWidth : DEFAULT_MIN_WIDTH,
-		minHeight: isNumber(rawOpts.minHeight) ? rawOpts.minHeight : DEFAULT_MIN_HEIGHT,
-		top: isNumber(rawOpts.top)
-			? rawOpts.top
-			: isNumber(rawOpts.bottom)
+		id: id && isString(id) ? id : null,
+		classname: (isString(classname) || Array.isArray(classname))
+			? classname
+			: defaultOpts.classname,
+		showHeader: isBoolean(showHeader) ? showHeader : defaultOpts.showHeader,
+		showActions: isBoolean(showActions) ? showActions : defaultOpts.showActions,
+		showClose: isBoolean(showClose) ? showClose : defaultOpts.showClose,
+		showMinimize: isBoolean(showMinimize) ? showMinimize : defaultOpts.showMinimize,
+		showMaximize: isBoolean(showMaximize) ? showMaximize : defaultOpts.showMaximize,
+		toggleHeader: isBoolean(toggleHeader) ? toggleHeader : defaultOpts.toggleHeader,
+		toggleActions: isBoolean(toggleActions) ? toggleActions : defaultOpts.toggleActions,
+		minWidth: isNumber(minWidth) ? minWidth : defaultOpts.minWidth,
+		minHeight: isNumber(minHeight) ? minHeight : defaultOpts.minHeight,
+		top: isNumber(top)
+			? top
+			: isNumber(bottom)
 				? null
-				: DEFAULT_POSITION,
-		bottom: isNumber(rawOpts.bottom) ? rawOpts.bottom : null,
-		left: isNumber(rawOpts.left) && !isNumber(rawOpts.right) ? rawOpts.left : null,
-		right: isNumber(rawOpts.right) ? rawOpts.right : DEFAULT_POSITION,
+				: defaultOpts.top,
+		bottom: isNumber(bottom) ? bottom : defaultOpts.bottom,
+		left: isNumber(left) && !isNumber(right) ? left : defaultOpts.left,
+		right: isNumber(right) ? right : defaultOpts.right,
 	};
 }
